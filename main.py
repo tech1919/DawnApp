dd307bacaa32ca8cb28c127a7bdf7fa9bf6a3bdf
ofryma <ofry60000@gmail.com>
2022-07-05 21:01:43 +0300

    home buttons, question and diagnose page changes

--- main.py
+++ main.py
@@ -1,40 +1,38 @@
 from kivy.uix.screenmanager import ScreenManager, Screen ,WipeTransition, FadeTransition , NoTransition , SlideTransition , FallOutTransition , RiseInTransition
 from kivymd.app import MDApp
-from kivy.core.window import Window
 from kivy.lang.builder import Builder
-from kivy.config import Config
 from kivy.utils import get_color_from_hex
 from kivy.clock import Clock
-from kivymd.uix.boxlayout import MDBoxLayout
-from kivy.uix.gridlayout import GridLayout
-from kivymd.uix.list import IRightBodyTouch
 from kivy.uix.scrollview import ScrollView
-from kivymd.uix.label import MDLabel
-from kivymd.uix.card import MDCard
 from kivymd.app import MDApp
 from kivymd.uix.list import MDList, TwoLineAvatarIconListItem,OneLineListItem ,TwoLineListItem,ThreeLineListItem ,OneLineAvatarIconListItem ,ThreeLineAvatarIconListItem , IconLeftWidget , IconRightWidget
-from kivy.uix.widget import Widget
-
-
+import os.path
 
 
 class DemoProject(ScreenManager):
     pass
 
-
 class DawnApp(MDApp):
-    def on_enter(self, *args):
-        Clock.schedule_once(self.switch_to_home, 5)
 
-    def change_ans(self , q_num):
-        cur_ans = self.answers[q_num]
-        if cur_ans == 'Yes':
-            self.answers[q_num] = 'No'
-        else:
-            self.answers[q_num] = 'Yes'
-
-
+################################# LOGIN PAGE ############################################
+    def hello(self,say_something):
+        print(say_something)
+    def google_login(self):
+        print("Google login")
+    def facebook_login(self):
+        print("Facebook login")
+    def forgot_password(self):
+        print("forgot password")
+    def login(self,*args):
+        screen_manager.current = "login"
+#########################################################################################
+################################# PROFILE PAGE ##########################################
+    def profile_changes(self,check):
+        print(f'{check} changes made')
     def create_scrollview(self):
+    # This function create a scroll view for the profile_diagnoseMe page
+    # It uses the self.answers dictionary and displays the questions and the answers
+    # of the user in the profile_diagnoseMe page
 
         if self.first_scroll_view:
             self.first_scroll_view = False
@@ -63,117 +61,17 @@ class DawnApp(MDApp):
             ml.add_widget(item)
 
         screen.ids['diagnose_me'].add_widget(sv)
-
-    def create_datepicker(self):
-        screen = screen_manager.get_screen('question_details_datepick')
-        sv = ScrollView()
-        ml = MDList()
-        sv.add_widget(ml)
-
-        for i in range(1,31):
-            item = OneLineListItem(
-                text = f'{i}',
-
-
-            )
-            ml.add_widget(item)
-
-
-
-
-    def go_to(self,screen_name):
-        # saves the last screen before changing
-
-
-        try:
-            if screen_name == 'back':
-                screen_name = self.last_screen
-        except:
-            pass
-        finally:
-            if not self.last_screen == screen_name:
-                self.last_screen = screen_manager.current
-
-
-        # screen = screen_manager.get_screen(screen_name)
-
-        try:
-
-            if screen_name == 'question':
-                if self.last_screen == 'login':
-                    self.root.transition = SlideTransition(direction='up')
-                    self.username = screen_manager.get_screen('login').ids.username_login.text
-                    self.password = screen_manager.get_screen('login').ids.password_login.text
-                else:
-                    self.root.transition = SlideTransition(direction='left')
-
-                screen_manager.current = screen_name
-            elif screen_name == 'question_details':
-                self.root.transition = NoTransition()
-                screen_manager.current = screen_name
-            elif screen_name == 'question_details_datepick':
-                self.root.transition = NoTransition()
-                screen_manager.current = screen_name
-            elif screen_name == 'profile':
-
-                if self.last_screen == 'question_details':
-                    screen = screen_manager.get_screen('question_details')
-                    self.weight = screen.ids.weight_input.text
-                    self.height = screen.ids.height_input.text
-                    self.change_text('weight_field',f'{self.weight} kg',screen_name)
-                    self.change_text('height_field', f'{self.height} cm',screen_name)
-                    screen_manager.current = screen_name
-                else:
-                    screen_manager.current = screen_name
-            elif screen_name == 'profile_diagnoseMe' or screen_name == 'profile_security':
-                screen = screen_manager.get_screen(screen_name)
-                screen_manager.current = screen_name
-
-                self.create_scrollview()
-
-
-                screen.ids['profile_btn'].source = 'profile_p.png'
-
-
-
-            elif screen_name == 'next_question' or screen_name == 'prev_question':
-
-
-                screen_name = self.render_question(screen_name)
-
-
-
-
-
-            elif screen_name == 'home':
-                screen_manager.current = screen_name
-            elif screen_name == 'login' or screen_name == 'signup':
-                screen = self.getScreen(screen_name)
-                screen.ids[f'username_{screen_name}'].text = ''
-                screen.ids[f'password_{screen_name}'].text = ''
-                if screen_name == 'signup':
-                    screen.ids[f'email_{screen_name}'].text = ''
-                screen_manager.current = screen_name
-            else:
-                self.root.transition = NoTransition()
-                screen_manager.current = screen_name
-        except:
-            pass
-            # screen_manager.current = 'profile'
-
-        self.change_navbar(screen_name)
-
-    def hello(self,say_something):
-        print(say_something)
-    def google_login(self):
-        print("Google login")
-    def facebook_login(self):
-        print("Facebook login")
-    def forgot_password(self):
-        print("forgot password")
-    def profile_changes(self,check):
-        print(f'{check} changes made')
+#########################################################################################
+################################# DIAGNOSE FUNCTIONS ####################################
+    def diagnose(self ,*args):
+        self.root.transition = FadeTransition()
+        screen_manager.current = "diagnose"
+        self.root.transition = NoTransition()
+#########################################################################################
+################################# QUESTION PAGE #########################################
     def on(self,check):
+        # This function controls the buttons in the question page
+
         screen_name = 'question'
         screen = screen_manager.get_screen(screen_name)
 
@@ -201,17 +99,22 @@ class DawnApp(MDApp):
 
         except:
             pass
-
     def render_question(self,operation):
+
+        # if the app go to a question page from a login or signup page
+        # the question current number need to reset for starting over
         if self.last_screen == 'login' or self.last_screen == 'signup':
             self.cur_question_idx = 0
 
         screen_name = 'question'
+
+        # this operations comes from the user's buttons in the question page
         if operation == 'next_question':
             self.cur_question_idx += 1
         elif operation == 'prev_question' and self.cur_question_idx > 0:
             self.cur_question_idx -= 1
 
+        # if the user finished all of the questions
         if self.cur_question_idx >= self.number_of_questions:
             screen_name = 'question_details'
             screen_manager.current = screen_name
@@ -225,33 +128,29 @@ class DawnApp(MDApp):
         self.change_text(f'question_text', self.cur_question, screen_name)
         self.change_text(f'question_number', f'QUESTION {self.cur_question_idx + 1} OF {self.number_of_questions}',
                          screen_name)
-        try:
-            self.change_photo(f'count', f'count_{self.cur_question_idx + 1}.png', screen_name)
-        except:
-            self.change_photo(f'count', f'count_9.png', screen_name)
 
-        try:
+        # This part switch the count number at the top of the question page
+        # to activate simply uncomment this part down here and the part in the question.kv
+        # file with the id of 'count_{number}'
+
+        # try:
+        #     self.change_photo(f'count', f'count_{self.cur_question_idx + 1}.png', screen_name)
+        # except:
+        #     self.change_photo(f'count', f'count_9.png', screen_name)
+
+
+        # check if there is an image for the current question
+        file_exists = os.path.exists(f'Q{self.cur_question_idx + 1}.png')
+        if file_exists:
             self.change_photo(f'question_img', f'Q{self.cur_question_idx + 1}.png', screen_name)
-        except:
+        else:
             self.change_photo(f'question_img', f'Q1.png', screen_name)
 
+
+        # reset the buttons
         self.on('reset')
 
         return screen_name
-
-    def getScreen(self,screen_name):
-        return screen_manager.get_screen(screen_name)
-    def change_text(self, id , text , screen_name):
-        # This function can change the text for a label in a given screen
-        screen = screen_manager.get_screen(screen_name)
-        screen.ids[id].text = text
-    def change_photo(self, id , new_path , screen_name):
-        # This function can change an image that has a id
-        # in a given screen
-
-        screen = screen_manager.get_screen(screen_name)
-        screen.ids[id].source = new_path
-
     def first_question(self,first=False):
         # This function sets up the questions list and the answers dictionary
 
@@ -288,6 +187,29 @@ class DawnApp(MDApp):
                 self.answers[i] = 'No'
 
         self.cur_question = self.questions[self.cur_question_idx]
+    def change_ans(self , q_num):
+        #### NOT WORKING ###
+        # Function for changing the answer for a certain question from the profile page
+
+        cur_ans = self.answers[q_num]
+        if cur_ans == 'Yes':
+            self.answers[q_num] = 'No'
+        else:
+            self.answers[q_num] = 'Yes'
+#########################################################################################
+################################# GENERAL FUNCTIONS #####################################
+    def getScreen(self,screen_name):
+        return screen_manager.get_screen(screen_name)
+    def change_text(self, id , text , screen_name):
+        # This function can change the text for a label in a given screen
+        screen = screen_manager.get_screen(screen_name)
+        screen.ids[id].text = text
+    def change_photo(self, id , new_path , screen_name):
+        # This function can change an image that has a id
+        # in a given screen
+
+        screen = screen_manager.get_screen(screen_name)
+        screen.ids[id].source = new_path
     def change_navbar(self,screen_name):
         # This function controls the bottom navigation bar
         # for a given screen_name, the funtion will switch the
@@ -309,6 +231,87 @@ class DawnApp(MDApp):
             screen.ids[f'{id}_btn'].source = f'{id}.png'
 
         screen.ids[f'{screen_name}_btn'].source = f'{screen_name}_p.png'
+    def go_to(self,screen_name):
+        # saves the last screen before changing
+
+        self.root.transition = NoTransition()
+
+        try:
+            if screen_name == 'back':
+                screen_name = self.last_screen
+        except:
+            pass
+        finally:
+            if not self.last_screen == screen_name:
+                self.last_screen = screen_manager.current
+
+
+        # screen = self.getScreen(screen_name)
+
+        try:
+
+            if screen_name == 'question_details':
+                self.root.transition = NoTransition()
+                screen_manager.current = screen_name
+            elif screen_name == 'question_details_datepick':
+                self.root.transition = NoTransition()
+                screen_manager.current = screen_name
+            elif screen_name == 'diagnose':
+                screen = self.getScreen('presplash_diagnose')
+                screen.ids[f'{screen_name}_btn'].source = f'{screen_name}.png'
+                screen_manager.current = 'presplash_diagnose'
+                Clock.schedule_once(self.diagnose, 2)
+            elif screen_name == 'profile':
+
+                if self.last_screen == 'question_details':
+                    screen = screen_manager.get_screen('question_details')
+                    self.weight = screen.ids.weight_input.text
+                    self.height = screen.ids.height_input.text
+                    self.change_text('weight_field',f'{self.weight} kg',screen_name)
+                    self.change_text('height_field', f'{self.height} cm',screen_name)
+                    screen_manager.current = screen_name
+                else:
+                    screen_manager.current = screen_name
+            elif screen_name == 'profile_diagnoseMe' or screen_name == 'profile_security':
+                screen = screen_manager.get_screen(screen_name)
+                screen_manager.current = screen_name
+
+                self.create_scrollview()
+
+
+                screen.ids['profile_btn'].source = 'profile_p.png'
+            elif screen_name == 'next_question' or screen_name == 'prev_question' or screen_name == 'question':
+
+                if self.last_screen == 'login':
+                    self.root.transition = SlideTransition(direction='up')
+                    self.username = screen_manager.get_screen('login').ids.username_login.text
+                    self.password = screen_manager.get_screen('login').ids.password_login.text
+                else:
+                    self.root.transition = SlideTransition(direction='left')
+
+
+                screen_name = self.render_question(screen_name)
+                screen_manager.current = screen_name
+            elif screen_name == 'home':
+                screen_manager.current = screen_name
+            elif screen_name == 'login' or screen_name == 'signup':
+                screen = self.getScreen(screen_name)
+                screen.ids[f'username_{screen_name}'].text = ''
+                screen.ids[f'password_{screen_name}'].text = ''
+                if screen_name == 'signup':
+                    screen.ids[f'email_{screen_name}'].text = ''
+                screen_manager.current = screen_name
+            else:
+                self.root.transition = NoTransition()
+                screen_manager.current = screen_name
+        except:
+            # in case on an error, the app go to profile page
+            print(f'error in {screen_name}')
+            screen_manager.current = 'profile'
+
+        self.change_navbar(screen_name)
+#########################################################################################
+################################# BUILD FUNCTIONS #######################################
     def build(self):
 
         # set the first question in line
@@ -328,6 +331,7 @@ class DawnApp(MDApp):
         screen_manager.add_widget(Builder.load_file('profile_diagnoseMe.kv'))
         screen_manager.add_widget(Builder.load_file('profile_security.kv'))
         screen_manager.add_widget(Builder.load_file('diagnose.kv'))
+        screen_manager.add_widget(Builder.load_file('presplash_diagnose.kv'))
         screen_manager.add_widget(Builder.load_file('question.kv'))
         screen_manager.add_widget(Builder.load_file('question_details.kv'))
         screen_manager.add_widget(Builder.load_file('question_details_datepick.kv'))
@@ -340,9 +344,8 @@ class DawnApp(MDApp):
     def on_start(self):
         # This function waits for 3 seconds in the presplash screen
         # until switching to the login screen
-        Clock.schedule_once(self.login , 3)
-    def login(self,*args):
-        screen_manager.current = "login"
+        Clock.schedule_once(self.login , 0)
+#########################################################################################
 
 
 if __name__ == '__main__':
